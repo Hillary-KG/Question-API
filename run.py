ef3a134f0810a234331b5cd4d5fd9a08f6f7885d
hillary <gachukihillary@gmail.com>
2019-01-15 22:04:09 +0300

    [finishes #162981858] [finishes #162981890] added endpoints to post, upvote or downvote a question

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import os
 
-from app import create_app
+from api import create_app
 
 config_name = os.getenv("APP_SETTINGS")
-# print("config name here ",config_name)
 app = create_app(config_name)
 
 if __name__ == "__main__":
