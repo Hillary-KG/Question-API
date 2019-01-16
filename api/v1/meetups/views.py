b8925f149ef70271c3fcbb4b36bfab684dee4103
hillary <gachukihillary@gmail.com>
2019-01-16 21:51:47 +0300

    bug fix

--- api/v1/meetups/views.py
+++ api/v1/meetups/views.py
@@ -2,27 +2,32 @@ from flask import jsonify,request
 from .models import Rsvp, Meetup
 from . import rsvp_blueprint
 
-@rsvp_blueprint.route('meetups/<int:meetup_id>/rsvps/',methods=['POST'])
+@rsvp_blueprint.route('/meetups/<int:meetup_id>/rsvps/',methods=['POST'])
 def rsvp_for_meetup(meetup_id):
     print("ID I am here",meetup_id)
     meetup = Meetup.get_meetup(meetup_id)
     user = request.json["user"]
     if user and meetup:
         data = {
-            "id":request.json["id"],
             "meetup":meetup,    
             "response":request.json["response"],
             "user":user
         }
-        return jsonify({
-                        "status":201,
-                        "data":data
-                        }), 201
+        rsvp = Rsvp().rsvp_meetup(**data)
+        if rsvp:
+            return jsonify({
+                            "status":201,
+                            "data":data
+                            }), 201
+        else:
+            return jsonify({
+                        "status":204,
+                        "error":"RSVP submission failed.No data submitted"
+                        }), 204
     else:
         return jsonify({
                         "status":404,
-                        "data":"User or Meetup Not Found"
+                        "error":"User or Meetup Not Found"
                         }), 404
-
-
+    
     
